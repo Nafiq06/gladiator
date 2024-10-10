77f5ce8a37096c279b01aca84672e70812a6979c
Nafiq Hoque <nafiq.hoque@learnet.se>
2024-10-10 12:59:22 +0200

    version ett

--- projectgladiator.py
+++ projectgladiator.py
@@ -11,7 +11,7 @@ while running:
     computer = random.choice(options)
 
     while player not in options:
-        player = input("Enter a choice (slag, kast, spark): ")
+        player = input("Enter a choice (slag, kast, spark, rock): ")
 
     print(f"Player: {player}")
     print(f"Computer: {computer}")
@@ -31,3 +31,33 @@ while running:
         running = False
 
 print("Tack för du spelade!")
+print("Now you're playing as Red gladiator and you'll brawl against gladiator blue")
+print("you'll fight in the Valhallah nummer 1 you can't die if you do you'll become")
+print("Towns biggest loser")
+running = True
+
+
+while running:
+
+    player = None
+    computer = random.choice(options)
+
+    while player not in options:
+        player = input("Enter a choice (slag, kast, spark, rock): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("Det står lika!")
+    elif player == "rock" and computer == "spark":
+        print("Du slog din fiende 80%!")
+    elif player == "paper" and computer == "kast":
+        print("Du slog din fiende 60%!")
+    elif player == "scissors" and computer == "slag":
+        print("Du vann grattis!")
+    else:
+        print("Du fölorat och din rike bränn ner!")
+
+    if not input("Spela igen? (y/n): ").lower() == "y":
+        running = False
